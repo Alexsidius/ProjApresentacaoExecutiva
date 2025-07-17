43fe354916c9d047c6f5a38c92f4ef21d18af8b1
Alexandro Ferreira <alexanndro@gmail.com>
2025-07-17 13:19:16 -0300

    PreparacaoInicial
    importei bibliotecas e salvei arquivos fornecidos pelo curso

---
+++ codigo_fonte.py
@@ -0,0 +1,7 @@
+#codigo de projeto em Python em ciencia de dados
+#usando numPy, Pandas e MatPlotLib
+#Autor: Alexandro Ferreira
+
+import pandas as pd
+base = pd.read_csv('base_dados.csv')
+print(base)
